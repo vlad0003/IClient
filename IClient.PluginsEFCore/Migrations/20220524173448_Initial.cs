e0ef806b4224eb4e5b6f975a4e136703a597b129
vlad003 <vlad0303200312@bk.ru>
2022-05-25 11:11:42 +0500

    Добавлена авторизация и аутентификация

---
+++ IClient.PluginsEFCore/Migrations/20220524173448_Initial.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace IClient.PluginsEFCore.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Inventories",
+                columns: table => new
+                {
+                    InventoryId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Inventoryname = table.Column<string>(type: "text", nullable: false),
+                    Quantity = table.Column<int>(type: "integer", nullable: false),
+                    Price = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inventories", x => x.InventoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ProductName = table.Column<string>(type: "text", nullable: false),
+                    Quantity = table.Column<int>(type: "integer", nullable: false),
+                    Price = table.Column<double>(type: "double precision", nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InventoryTransactions",
+                columns: table => new
+                {
+                    InventoryTransactionId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ActivityType = table.Column<int>(type: "integer", nullable: false),
+                    InventoryId = table.Column<int>(type: "integer", nullable: false),
+                    QuantityBefore = table.Column<int>(type: "integer", nullable: false),
+                    QuantityAfter = table.Column<int>(type: "integer", nullable: false),
+                    TransactionDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DoneBy = table.Column<string>(type: "text", nullable: false),
+                    PONumber = table.Column<string>(type: "text", nullable: true),
+                    ProductionNumber = table.Column<string>(type: "text", nullable: true),
+                    UnitPrice = table.Column<double>(type: "double precision", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InventoryTransactions", x => x.InventoryTransactionId);
+                    table.ForeignKey(
+                        name: "FK_InventoryTransactions_Inventories_InventoryId",
+                        column: x => x.InventoryId,
+                        principalTable: "Inventories",
+                        principalColumn: "InventoryId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductInventory",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(type: "integer", nullable: false),
+                    InventoryId = table.Column<int>(type: "integer", nullable: false),
+                    InventoryQuantity = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductInventory", x => new { x.ProductId, x.InventoryId });
+                    table.ForeignKey(
+                        name: "FK_ProductInventory_Inventories_InventoryId",
+                        column: x => x.InventoryId,
+                        principalTable: "Inventories",
+                        principalColumn: "InventoryId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductInventory_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductTransactions",
+                columns: table => new
+                {
+                    ProductTransactionId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ActivityType = table.Column<int>(type: "integer", nullable: false),
+                    ProductId = table.Column<int>(type: "integer", nullable: false),
+                    QuantityBefore = table.Column<int>(type: "integer", nullable: false),
+                    QuantityAfter = table.Column<int>(type: "integer", nullable: false),
+                    TransactionDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DoneBy = table.Column<string>(type: "text", nullable: false),
+                    SalesOrderNumber = table.Column<string>(type: "text", nullable: true),
+                    ProductionNumber = table.Column<string>(type: "text", nullable: true),
+                    UnitPrice = table.Column<double>(type: "double precision", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductTransactions", x => x.ProductTransactionId);
+                    table.ForeignKey(
+                        name: "FK_ProductTransactions_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Inventories",
+                columns: new[] { "InventoryId", "Inventoryname", "Price", "Quantity" },
+                values: new object[,]
+                {
+                    { 1, "Gas Engine", 1000.0, 1 },
+                    { 2, "Body", 400.0, 1 },
+                    { 3, "Wheel", 100.0, 4 },
+                    { 4, "Seat", 50.0, 5 },
+                    { 5, "Electric Engine", 8000.0, 2 },
+                    { 6, "Battery", 400.0, 5 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "IsActive", "Price", "ProductName", "Quantity" },
+                values: new object[,]
+                {
+                    { 1, true, 20000.0, "Gas Car", 1 },
+                    { 2, true, 15000.0, "Electric Car", 1 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ProductInventory",
+                columns: new[] { "InventoryId", "ProductId", "InventoryQuantity" },
+                values: new object[,]
+                {
+                    { 1, 1, 1 },
+                    { 2, 1, 1 },
+                    { 3, 1, 4 },
+                    { 4, 1, 5 },
+                    { 2, 2, 1 },
+                    { 3, 2, 4 },
+                    { 4, 2, 5 },
+                    { 5, 2, 1 },
+                    { 6, 2, 1 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InventoryTransactions_InventoryId",
+                table: "InventoryTransactions",
+                column: "InventoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductInventory_InventoryId",
+                table: "ProductInventory",
+                column: "InventoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductTransactions_ProductId",
+                table: "ProductTransactions",
+                column: "ProductId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InventoryTransactions");
+
+            migrationBuilder.DropTable(
+                name: "ProductInventory");
+
+            migrationBuilder.DropTable(
+                name: "ProductTransactions");
+
+            migrationBuilder.DropTable(
+                name: "Inventories");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
