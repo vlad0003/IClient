ca8ed4717b629e68ba0d766d1171743ad4f5c013
vlad003 <vlad0303200312@bk.ru>
2022-05-26 11:58:11 +0500

    Редактирование пользовательского интерфейса

--- IClient/Areas/Identity/RevalidatingIdentityAuthenticationStateProvider.cs
+++ IClient/Areas/Identity/RevalidatingIdentityAuthenticationStateProvider.cs
@@ -1,5 +1,4 @@
 using System.Security.Claims;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Server;
 using Microsoft.AspNetCore.Identity;
